d2fb7f278f61f53494986d03b926a13cf3dc92a2
Dimitrij Denissenko <contact@dvisionfactory.com>
2009-05-14 08:09:15 +0100

    Added patch for OpenID & Ruby 1.9

--- ext_info.rb
+++ ext_info.rb
@@ -1,14 +1,16 @@
 #--
-# Copyright (C) 2008 Dimitrij Denissenko
+# Copyright (C) 2009 Dimitrij Denissenko
 # Please read LICENSE document for more information.
 #++
-gem 'ruby-openid', '>= 2.0.0'
+gem 'ruby-openid', '>= 2.2.6'
 require 'openid'
 require 'openid/store/filesystem'
 require 'openid/extensions/sreg'
+require 'openid/patches'
 
 RetroEM::Views.register_extension 'openid_auth/user', :user, :fields
 RetroEM::Views.register_extension 'openid_auth/session/form', :session, :top
 RetroEM::Views.register_extension 'openid_auth/session/header', :session, :header
 RetroEM::Views.register_extension 'openid_auth/account/form', :user, :new, :top
 RetroEM::Views.register_extension 'openid_auth/account/header', :user, :new, :header
+
---
+++ lib/openid/patches.rb
@@ -0,0 +1,12 @@
+OpenID::OpenIDServiceEndpoint.class_eval do
+  
+  class << self
+
+    def from_html_with_unicode_patch(uri, html)
+      from_html_without_unicode_patch(uri, html.force_encoding('UTF-8'))      
+    end
+    alias_method_chain :from_html, :unicode_patch
+    
+  end
+
+end if RUBY_VERSION.to_f >= 1.9
